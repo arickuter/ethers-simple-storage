b920791f786c5607c708ba76d4beac933991eaa6
Aric Kuter <arickuter99@gmail.com>
2022-08-24 13:56:06 +0200

    encrypted key

--- deploy.js
+++ deploy.js
@@ -4,8 +4,13 @@ require("dotenv").config();
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-
+  // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf-8");
+  let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    encryptedJson,
+    process.env.PRIVATE_KEY_PASSWORD
+  );
+  wallet = await wallet.connect(provider);
   const abi = fs.readFileSync(
     "./contracts/compiled/SimpleStorage_sol_SimpleStorage.abi",
     "utf-8"
